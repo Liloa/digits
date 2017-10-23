f46d78b19f35410181927bb42d6e92065e211575
Liloa <dalocalman@gmail.com>
2017-10-22 23:15:50 -1000

    Final commit

--- app/imports/startup/client/router.js
+++ app/imports/startup/client/router.js
@@ -15,13 +15,13 @@ FlowRouter.route('/list', {
   },
 });
 
-FlowRouter.route('/add-contact', {
+FlowRouter.route('/add-contact/:_id', {
   name: 'Add_Contact_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Add_Contact_Page' });
   },
 });
-FlowRouter.route('/edit-contact', {
+FlowRouter.route('/edit-contact/:_id', {
   name: 'Edit_Contact_Page',
   action() {
     BlazeLayout.render('App_Body', { main: 'Edit_Contact_Page' });
